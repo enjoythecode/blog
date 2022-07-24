75789e779c07c9abb8128f47fc026b93585e51f7
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-07-24 16:55:51 -0700

    list of posts in index.html
    - no metadata (yet)
    - ultra-simple, which I do not mind

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,23 @@
 main (){
-    find src -name "*.md" | sed -e 's/src\/\(.*\).md/\1/' | md_to_html
+	# delete lines after the given pattern in index.md
+	# the pattern is where I place my list of posts
+	# -i means sed will edit the file in-place!
+	sed '1,/SINAN_CUSTOM_INSERT_LIST_OF_POSTS/!d' src/index.md > src/index.md.tmp
+
+	while read -r line; do
+		if [ "$line" != "index" ]; then
+			printf "\n\n- [$line]($line.html)" >> src/index.md.tmp
+		fi
+	done < <(list_src_files)
+
+	cat src/index.md.tmp > src/index.md
+	rm src/index.md.tmp
+
+    list_src_files | md_to_html
+}
+
+list_src_files() {
+	find src -name "*.md" | sed -e 's/src\/\(.*\).md/\1/'
 }
 
 md_to_html() {
--- src/index.md
+++ src/index.md
@@ -1,3 +1,21 @@
 # My Blog
 
-This is where I write down some of my ideas to share with people interested.
+This is where I write down some of my ideas to share.
+
+## Posts
+<!-- SINAN_CUSTOM_INSERT_LIST_OF_POSTS -->
+
+
+- [communication](communication.html)
+
+- [another](another.html)
+
+- [umbrella](umbrella.html)
+
+- [buildablog](buildablog.html)
+
+- [offline](offline.html)
+
+- [better_kb_practice](better_kb_practice.html)
+
+- [getting_thoughts_out](getting_thoughts_out.html)
